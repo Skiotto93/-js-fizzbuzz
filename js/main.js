747fb2f238671d17311b676b3d987835a92cddc9
skiotto93 <fabio.forenza@hotmail.com>
2022-10-24 17:17:33 +0200

    Aggiunti inner.html  a tutti gli elementi e style

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const list = document.querySelector("list")
+const list = document.querySelector(".list")
 
 // Creo un programma che stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
