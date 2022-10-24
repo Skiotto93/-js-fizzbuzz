1a275cbbd766f57efe0c1b4b220580cbaf1cda25
skiotto93 <fabio.forenza@hotmail.com>
2022-10-24 16:43:51 +0200

    Aggiunte parti grafiche

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const list = document.querySelector(".list")
+const list = document.querySelector("list")
 
 // Creo un programma che stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
@@ -11,16 +11,22 @@ for (let i = 1; i <= 100; i++) {
     //Per i multipli di 3 e di 5 stampo, in console, la stringa "fizzbuzz"
     if ( i % 3 === 0 && i % 5 === 0 ) {
         item.classList.add("fizzbuzz");
+        item.innerHTML = "fizzbuzz";
         console.log("fizzbuzz");
     //Creo per i multipli di 3 l'item "fizz" e lo stampo in console
     } else if ( i % 3 === 0 ) {
         item.classList.add("fizz");
+        item.innerHTML = "fizz";
         console.log("fizz");
     //Creo per i multipli di 5 l'item "buzz" e lo stampo in console
     } else if ( i % 5 === 0 ) {
         item.classList.add("Buzz");
+        item.innerHTML = "Buzz";
         console.log("Buzz");
     } else {
         console.log(i);
+        item.innerHTML = i;
     }
+    //Inserisco l'item nella lista attraverso .append
+    list.append(item);
 }
