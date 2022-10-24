5f454021c247ee49290cf227d2552395bb24ee80
skiotto93 <fabio.forenza@hotmail.com>
2022-10-24 15:33:54 +0200

    Aggiunta condizione per multipli di 5

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const list = document.querySelector(".list")
 
 // Creo un programma che stampi in console i numeri da 1 a 100
-for (let i = 1; i < 101; i++) {
+for (let i = 1; i <= 100; i++) {
     //Creo l'elemento
     const item = document.createElement("li");
     //Aggiunto classe item
@@ -13,5 +13,13 @@ for (let i = 1; i < 101; i++) {
     if ( i % 3 === 0 && i % 5 === 0 ) {
         item.classList.add("fizzbuzz");
         console.log("fizzbuzz");
+    //Creo per i multipli di 3 l'item "fizz" e lo stampo in console
+    } else if ( i % 3 === 0 ) {
+        item.classList.add("fizz");
+        console.log("fizz");
+    //Creo per i multipli di 5 l'item "buzz" e lo stampo in console
+    } else if ( i % 5 === 0 ) {
+        item.classList.add("Buzz");
+        console.log("fizz");
     }
 }
